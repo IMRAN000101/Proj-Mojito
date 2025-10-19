5cb2317b63eb39590ab39465b4bd43957f4ffce4
I_m_r_a_n <md9020096@gmail.com>
2025-10-20 00:17:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+import gsap from 'gsap'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+// import { useGSAP } from '@gsap/react'
+// import {ScrollTrigger, splitText} from 'gsap/all'
+
+// gsap.registerPlugin(scrollTrigger, splitText)
+
+const App = () => {
+  return (
+    <main>
+      <Navbar/> 
+       <Hero/>
+       <div className='h-dvh bg-black'></div>
+    </main>
+  )
+}
+
+export default App
