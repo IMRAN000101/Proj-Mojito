65f7202ce1cc6a49392725dab79dd613b8bf5f0f
I_m_r_a_n <md9020096@gmail.com>
2025-10-20 13:30:02 +0530

    implemented art section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
+import Art from './components/Art'
 // import { useGSAP } from '@gsap/react'
 // import {ScrollTrigger, splitText} from 'gsap/all'
 
@@ -16,6 +17,7 @@ const App = () => {
        <Hero/>
        <Cocktails/>
        <About/>
+       <Art/>
     </main>
   )
 }
