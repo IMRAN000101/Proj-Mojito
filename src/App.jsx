29bb5c4c6ab7e1e1dc74ea8bf235bfdd8717b4ca
I_m_r_a_n <md9020096@gmail.com>
2025-10-20 11:52:13 +0530

    Implemented cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
 // import { useGSAP } from '@gsap/react'
 // import {ScrollTrigger, splitText} from 'gsap/all'
 
@@ -12,6 +13,7 @@ const App = () => {
     <main>
       <Navbar/> 
        <Hero/>
+       <Cocktails/>
     </main>
   )
 }
